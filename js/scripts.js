e659fd9fb9dce8b71a05457de3bb2aaa80732b5f
nathabonfim59 <nathabonfim59@gmail.com>
2017-09-04 00:56:01 -0300

    Add:
    Assign values to:
    
    -city
    -country
    -main
    
    Modify icons acording weather id:
    - rain
    - storm

--- js/scripts.js
+++ js/scripts.js
@@ -27,9 +27,115 @@ function animateButton(buttonId) {
     }
 }
 
+// Get the coordinates of user using geolocation API
+function getPosition() {
+    if (!navigator.geolocation) {
+        alert("Infortunatelly, your navigator don't support geolocation API");
+    } else {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            getWeather({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            });
+        });
+    }
+} 
+
+// Get the weather of user in your position
+function getWeather(coordinates) {
+    $.getJSON("https://fcc-weather-api.glitch.me/api/current?"+
+    "lat="+coordinates.latitude +
+    "&lon=" + coordinates.longitude, filterInfo);
+}
+
+// Filter and struct informations from JSON
+function filterInfo(JSON) {
+    var weatherInfo = {
+        id: JSON.weather[0].id,
+        main: JSON.weather[0].main,
+        temp: (JSON.main.temp).toFixed(1),
+        city: JSON.name,
+        country: JSON.sys.country,
+    }
+
+    assignValues(weatherInfo);
+}
+
+// Assign values to page
+function assignValues(weatherInfo) {
+    $("#cityName").text(
+        weatherInfo.city + ", " +
+        weatherInfo.country
+    );
+
+    $("#main").text(weatherInfo.main);
+
+    $("#curentTemperature").text(weatherInfo.temp);
+    
+    assignIcon(weatherInfo.id);
+}
+
+// Assign a icon according id
+// All conditionals are based on the doc of:
+// - WeatherIcons -> https://erikflowers.github.io/weather-icons/
+// - OpenWeatherMap -> https://openweathermap.org/weather-conditions
+function assignIcon(id) {
+    var iconClass = null;
+    if (id >= 200 && id < 300) {
+        switch(id) {
+            case 200:
+            case 201:
+            case 210:
+            case 212:
+            case 221:
+            case 230:
+            case 231:
+                iconClass = "wi-storm-showers";
+                break;
+            case 202:
+            case 211:
+            case 232:
+                iconClass = "wi-thunderstorm";
+                break;
+            default:
+                iconClass = "wi-storm-showers";
+                break;
+        }
+    } else if (id >= 300 && id < 400) {
+        switch (id) {
+            case 300:
+            case 301:
+            case 310:
+                iconClass = "wi-showers";
+                break;
+            case 302:
+            case 311:
+            case 313:
+                iconClass = "wi-sleet";
+                break;
+            case 312:
+            case 314:
+            case 321:
+                iconClass = "wi-rain-mix";
+                break;
+            default:
+                iconClass = "wi-showers"
+                break;
+        }
+    } else if (id >= 500 && id < 600) {
+        iconClass = "wi-rain";
+    } else if (id >= 600 && id < 700) {
+        iconClass = "wi-snow";
+    } else if (id >= 700) {
+
+    }
+}
+
 $(document).ready(function() {
     $("#changeTempScale").click(function(event) {
         var requestedScale = event.target.id;
         animateButton(requestedScale);
     });
+
+    getPosition();
 });
