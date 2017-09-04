9687a7d221e322e1983f4ff941edec52e44149f6
nathabonfim59 <nathabonfim59@gmail.com>
2017-09-03 23:02:02 -0300

    Some fixes
    styles - Pointer when hover change scale buttons
    
    scripts - Seperate animation of change scale buttons in a functon

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,35 @@
 // var $ = require('jquery');
+
+// Animate the click in change scale button
+function animateButton(buttonId) {
+    // Add the aparence of enabled when clicked
+    function clearActive(element, animationClass) {
+        $("#changeTempScale .active").removeClass("active")
+        $("#" + buttonId).addClass("active");
+
+        // Remove the previous animation class
+        if (element && animationClass) {
+            $(element).removeClass(animationClass);
+        }
+    }
+
+    function addAnimation(element, animationClass) {
+           $(element).addClass(animationClass);
+    }
+    // Make the conversion between scales
+    if (buttonId === "tempCelcius") {
+        clearActive("#tempFarenheit", "animate-right");
+        addAnimation("#tempCelcius", "animate-left");
+
+    } else if (buttonId === "tempFarenheit") {
+        clearActive("#tempCelcius", "animate-left");
+        addAnimation("#tempFarenheit", "animate-right");
+    }
+}
+
 $(document).ready(function() {
     $("#changeTempScale").click(function(event) {
         var requestedScale = event.target.id;
-        
-        // Add the aparence of enabled when clicked
-        function clearActive(element, animationClass) {
-            $("#changeTempScale .active").removeClass("active")
-            $("#" + requestedScale).addClass("active");
-
-            // Remove the previous animation class
-            if (element && animationClass) {
-                $(element).removeClass(animationClass);
-            }
-        }
-
-        function addAnimation(element, animationClass) {
-               $(element).addClass(animationClass);
-        }
-        // Make the conversion between scales
-        if (requestedScale === "tempCelcius") {
-            clearActive("#tempFarenheit", "animate-right");
-            addAnimation("#tempCelcius", "animate-left");
-
-        } else if (requestedScale === "tempFarenheit") {
-            clearActive("#tempCelcius", "animate-left");
-            addAnimation("#tempFarenheit", "animate-right");
-        }
+        animateButton(requestedScale);
     });
 });
